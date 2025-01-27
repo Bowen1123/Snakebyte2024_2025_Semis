f3afce328f1b85f56679ef6dc5de8a7516897849
Bowen1123 <131422130+Bowen1123@users.noreply.github.com>
2025-01-27 16:04:18 -0600

    we
    nm

--- MeepMeepTesting/src/main/java/org/firstinspires/ftc/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/org/firstinspires/ftc/meepmeeptesting/MeepMeepTesting.java
@@ -11,25 +11,48 @@ public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
-        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+        RoadRunnerBotEntity rightBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 16)
+                .setConstraints(25, 25, Math.toRadians(180), Math.toRadians(180), 18)
                 .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-30, -60, Math.toRadians(180)))
-                        .lineTo(new Vector2d(-50, -60))
+                        .setTangent(0)
+                        .lineTo(new Vector2d(-50, -60)) // Score pre-loaded at basket
+                        .addDisplacementMarker(() -> {
+
+                        })
                         .lineTo(new Vector2d(-34, -40))
                         .splineTo(new Vector2d(-34, -10), Math.toRadians(100))
                         .forward(5) // Pick up first element
-                        .lineTo(new Vector2d(-55, -55)) // Score at backet
-                        .lineTo(new Vector2d(-40, -40))
-                        .splineTo(new Vector2d(-48, -10), Math.toRadians(0))
+                        .addDisplacementMarker(() -> {
+
+                        })
+                        .lineTo(new Vector2d(-55, -55)) // Score at basket
+                        .addDisplacementMarker(() -> {
+
+                        })
+                        //.lineTo(new Vector2d(-40, -40))
+                        .splineToConstantHeading(new Vector2d(-48, -12), Math.toRadians(180))
+                        .forward(3) // Pick up second element
+                        .addDisplacementMarker(() -> {
+
+                        })
+                        .lineTo(new Vector2d(-55, -55)) // Score at basket
+                        .addDisplacementMarker(() -> {
+
+                        })
 
                         .build());
+        RoadRunnerBotEntity leftBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(25, 25, Math.toRadians(180), Math.toRadians(180), 18)
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(30, -60, Math.toRadians(180)))
 
+                        .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
+                .addEntity(rightBot)
                 .start();
     }
 }
