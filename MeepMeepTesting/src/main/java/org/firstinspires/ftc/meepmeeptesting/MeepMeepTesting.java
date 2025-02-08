40aff869b12263a7d6c0d838402fa44a29623aa9
Bowen1123 <131422130+Bowen1123@users.noreply.github.com>
2025-02-07 21:33:31 -0600

    2/7/2025

--- MeepMeepTesting/src/main/java/org/firstinspires/ftc/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/org/firstinspires/ftc/meepmeeptesting/MeepMeepTesting.java
@@ -11,70 +11,70 @@ public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
-        RoadRunnerBotEntity rightBot = new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(25, 25, Math.toRadians(180), Math.toRadians(180), 18)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-30, -60, Math.toRadians(180)))
-                        .setTangent(0)
-                        .lineTo(new Vector2d(-50, -60)) // Score pre-loaded at basket
-                        .addDisplacementMarker(() -> {
+//        RoadRunnerBotEntity rightBot = new DefaultBotBuilder(meepMeep)
+//                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+//                .setConstraints(25, 25, Math.toRadians(180), Math.toRadians(180), 18)
+//                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-30, -60, Math.toRadians(180)))
+//                        .setTangent(0)
+//                        .lineTo(new Vector2d(-50, -60)) // Score pre-loaded at basket
+//                        .addDisplacementMarker(() -> {
+//
+//                        })
+//                        .lineTo(new Vector2d(-34, -40))
+//                        .splineTo(new Vector2d(-34, -10), Math.toRadians(100))
+//                        .forward(5) // Pick up first element
+//                        .addDisplacementMarker(() -> {
+//
+//                        })
+//                        .lineTo(new Vector2d(-55, -55)) // Score at basket
+//                        .addDisplacementMarker(() -> {
+//
+//                        })
+//                        //.lineTo(new Vector2d(-40, -40))
+//                        .splineToConstantHeading(new Vector2d(-48, -12), Math.toRadians(180))
+//                        .forward(3) // Pick up second element
+//                        .addDisplacementMarker(() -> {
+//
+//                        })
+//                        .lineTo(new Vector2d(-55, -55)) // Score at basket
+//                        .addDisplacementMarker(() -> {
+//
+//                        })
+//                        .splineToConstantHeading(new Vector2d(-56, -12), Math.toRadians(180))
+//                        .forward(3) // Pick up second element
+//                        .addDisplacementMarker(() -> {
+//
+//                        })
+//                        .lineTo(new Vector2d(-55, -55)) // Score at basket
+//                        .addDisplacementMarker(() -> {
+//
+//                        })
+//
+//                        .build());
+//        RoadRunnerBotEntity leftBot = new DefaultBotBuilder(meepMeep)
+//                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+//                .setConstraints(25, 25, Math.toRadians(180), Math.toRadians(180), 18)
+//                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(30, 60, Math.toRadians(0)))
+//                        .lineTo(new Vector2d(33, 60))
+//                        .build());
+//        RoadRunnerBotEntity extra1 = new DefaultBotBuilder(meepMeep)
+//                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+//                .setConstraints(25, 25, Math.toRadians(180), Math.toRadians(180), 18)
+//                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-30, 60, Math.toRadians(0)))
+//                        .lineTo(new Vector2d(33, 60))
+//                        .build());
+//        RoadRunnerBotEntity extra2 = new DefaultBotBuilder(meepMeep)
+//                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+//                .setConstraints(25, 25, Math.toRadians(180), Math.toRadians(180), 18)
+//                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(30, -60, Math.toRadians(0)))
+//                        .lineTo(new Vector2d(33, 60))
+//                        .build());
 
-                        })
-                        .lineTo(new Vector2d(-34, -40))
-                        .splineTo(new Vector2d(-34, -10), Math.toRadians(100))
-                        .forward(5) // Pick up first element
-                        .addDisplacementMarker(() -> {
 
-                        })
-                        .lineTo(new Vector2d(-55, -55)) // Score at basket
-                        .addDisplacementMarker(() -> {
-
-                        })
-                        //.lineTo(new Vector2d(-40, -40))
-                        .splineToConstantHeading(new Vector2d(-48, -12), Math.toRadians(180))
-                        .forward(3) // Pick up second element
-                        .addDisplacementMarker(() -> {
-
-                        })
-                        .lineTo(new Vector2d(-55, -55)) // Score at basket
-                        .addDisplacementMarker(() -> {
-
-                        })
-                        .splineToConstantHeading(new Vector2d(-56, -12), Math.toRadians(180))
-                        .forward(3) // Pick up second element
-                        .addDisplacementMarker(() -> {
-
-                        })
-                        .lineTo(new Vector2d(-55, -55)) // Score at basket
-                        .addDisplacementMarker(() -> {
-
-                        })
-
-                        .build());
-        RoadRunnerBotEntity leftBot = new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(25, 25, Math.toRadians(180), Math.toRadians(180), 18)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(30, 60, Math.toRadians(0)))
-                        .lineTo(new Vector2d(33, 60))
-                        .build());
-        RoadRunnerBotEntity extra1 = new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(25, 25, Math.toRadians(180), Math.toRadians(180), 18)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-30, 60, Math.toRadians(0)))
-                        .lineTo(new Vector2d(33, 60))
-                        .build());
-        RoadRunnerBotEntity extra2 = new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(25, 25, Math.toRadians(180), Math.toRadians(180), 18)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(30, -60, Math.toRadians(0)))
-                        .lineTo(new Vector2d(33, 60))
-                        .build());
-
-
-        meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
-                .setDarkMode(true)
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_LIGHT)
+                .setDarkMode(false)
                 .setBackgroundAlpha(0.95f)
-                .addEntity(rightBot)
+                //.addEntity(rightBot)
                 .start();
     }
 }
