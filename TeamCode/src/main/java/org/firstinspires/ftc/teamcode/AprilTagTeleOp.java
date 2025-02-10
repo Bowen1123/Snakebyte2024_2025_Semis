6660361991d9af3b79670d45aa8d0201a7c46912
Bowen1123 <131422130+Bowen1123@users.noreply.github.com>
2025-02-10 17:38:19 -0600

    cam

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagTeleOp.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+
+public class AprilTagTeleOp extends LinearOpMode {
+
+    WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanism/MyCamera.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.Mechanism;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.opencv.core.Mat;
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+// Used for Object Detection not Apriltags, that's somewhere else
+
+
+public class MyCamera {
+    private OpenCvWebcam camera;
+    private OpenCvPipeline pipeline_1;
+
+    public MyCamera(HardwareMap hardwareMap){
+        camera = hardwareMap.get(OpenCvWebcam.class, "Webcam 1");
+    }
+
+    public void setPipeline_1(){
+        camera.setPipeline(pipeline_1);
+    }
+
+}
+
+
+
+class drawRectangle extends OpenCvPipeline{
+
+    @Override
+    public Mat processFrame(Mat input) {
+
+
+
+        return null;
+    }
+}
