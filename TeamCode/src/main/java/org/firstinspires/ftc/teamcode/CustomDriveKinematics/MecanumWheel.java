fb26e5cde2dc92ad33bcda336c6e497d2e314887
Harshid <harshid.kkunka@gmail.com>
2025-02-17 12:43:00 -0600

    Add dcMotor to wheel class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CustomDriveKinematics/MecanumWheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CustomDriveKinematics/MecanumWheel.java
@@ -1,13 +1,26 @@
 package org.firstinspires.ftc.teamcode.CustomDriveKinematics;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public class MecanumWheel {
     private final double locationX;
     private final double locationY;
     private final double wheelRadius;
-    MecanumWheel(double XOffset, double YOffset, double wheelRadius){
+    private final DcMotor motor;
+    MecanumWheel(double XOffset, double YOffset, double wheelRadius, DcMotor motor){
         this.locationX = XOffset;
         this.locationY = YOffset;
         this.wheelRadius = wheelRadius;
+        this.motor = motor;
+    }
+    public void setMotorPosition(int position){
+        motor.setTargetPosition(position);
+    }
+    public void setMotorPower(double power){
+        motor.setPower(power);
+    }
+    public void setMotorRunMode(DcMotor.RunMode mode){
+        motor.setMode(mode);
     }
     public double getLocationX() {
         return locationX;
@@ -17,5 +30,11 @@ public class MecanumWheel {
     }
     public double getWheelRadius() {
         return wheelRadius;
+    }
+    public double getMotorPower(){
+        return motor.getPower();
+    }
+    public int getCurrentPosition(){
+        return motor.getCurrentPosition();
     }
 }
